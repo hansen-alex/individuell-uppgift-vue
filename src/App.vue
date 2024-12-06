48831a30e4d1b7c7c2517a3ca2f383da33e46924
Alex Hansen <hansen.alex@telia.com>
2024-12-06 16:17:19 +0100

    krav comment

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,29 @@
 <script setup lang="ts">
+/* TODO Krav
+-G:
+Det ska vara en Vue applikation [ ]
+Använda TypeScript [ ]
+Använda följande i koden: [ ]
+Textinterpolering {{ exempel }} [ ]
+Villkorlig rendering (v-if) [ ]
+Listrendering (v-for) [ ]
+Händelser (v-on eller dess kortform) [ ]
+Använda computed [ ]
+v-model [ ]
+minst två webbsidor med Vue Router [ ]
+minst en prop ska skickas ned till komponent  [ ]
+Props ska vara typsäkra. Exempel, ”type: number” [ ]
+Använda ref() eller data()... för att hantera reactive state [ ]
+Finnas Lifeycle hook (mounted) [ ]
+Webbanrop ska göras med fetch eller bibliotek för att hämta och visa data [ ]
+
+-VG:
+Applikationen är robust med genomtänkt felhantering och validering [ ]
+Använda dynamisk route matching för att hämta och visa data för ett specifikt element från webanropet, även vid refresh eller om sidan öppnas i ett nytt fönster (ex: recept.se/sommar-paj) [ ]
+Lägg till router guards för att blockera användares åtkomst till sida dem inte är behörig till [ ]
+Hantera sidor som inte finns (404) [ ]
+Använd provide / inject från root component till valfri child component med default värde som fallback [ ]
+*/
 </script>
 
 <template>
